296e6cc2099c38a31ef842273e205ccc485dcfff
Adrian Lucero <adrianlucerot@outlook.com>
2018-12-05 15:23:58 -0500

    Update configApp.php

--- configApp.php
+++ configApp.php
@@ -3,17 +3,17 @@
 /* Go to My Apps dashboard: https://developers.mercadolibre.com.ar/apps/home, and get the information you need in order to the following enviroment variables */
 
 /* Your Application Id */
-$appId = getenv('App_ID');
+$appId = "2659357390212416";
 
 /* Your Secret Key */
-$secretKey = getenv('Secret_Key');
+$secretKey = "uFVXTeMnOTjs2upY6zEO7BfmxSgSH7ZN";
 
 /* The Redirect url */
-$redirectURI = getenv('Redirect_URI');
+$redirectURI = "";
 
 /* The site id of the country where your application will work.
 If you don't know your site_id go to our sites resources: https://api.mercadolibre.com/sites  */
-$siteId = 'MLB';
+$siteId = 'MLA';
 
 
 
